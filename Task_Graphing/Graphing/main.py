9f339173fae601f9badc41fcb0d491b51e8abeff
Natalia <chekiska.11@mail.ru>
2024-07-15 21:22:06 +0500

    Внесла изменения в файл с задачей

--- Task_Graphing/Graphing/main.py
+++ Task_Graphing/Graphing/main.py
@@ -14,3 +14,21 @@ data = pd.DataFrame({'whoAmI':lst})
 data.head()
 '''
 
+import random
+import pandas as pd
+
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
+print(data.head())
+
+print(pd.get_dummies(data['whoAmI']))
+
+# Переводим указанный код в 'one hot' вид без get_dummies:
+
+data['robot'] = (data['whoAmI'] == 'robot').astype(int)
+data['human'] = (data['whoAmI'] == 'human').astype(int)
+
+print("One-hot Encoded DataFrame:")
+print(data.head())
